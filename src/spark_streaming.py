2226b590a95b5417f83d048fdc38e282bb86a42d
Nia <144040611+lnynhi02@users.noreply.github.com>
2024-10-03 18:53:04 +0700

    Update spark_streaming.py

--- src/spark_streaming.py
+++ src/spark_streaming.py
@@ -8,7 +8,7 @@ import os
 logging.basicConfig(level=logging.INFO)
 
 config = configparser.ConfigParser()
-config_path = os.path.join(os.path.dirname(__file__), "config", "config.ini")
+config_path = os.path.join(os.path.dirname(__file__), "..", "config", "config.ini")
 config.read(config_path)
 
 
